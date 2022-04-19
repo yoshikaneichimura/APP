4c554d59ed6ab25aed983c6d7013da7a9e9bb66c
EC2 Default User <ec2-user@ip-172-31-85-166.ec2.internal>
2022-04-19 10:35:16 +0000

    Initial Commit2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,29 @@
 class ListsController < ApplicationController
   def new
+    #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list=List.new
+  end
+  
+  def create
+  list=List.new(list_params)
+  list.save
+  redirect_to list_path(list.id)
   end
 
   def index
+    @lists=List.all
   end
 
   def show
+    @list=List.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+  
+   def list_params
+     params.require(:list).permit(:title,:body)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
+  post 'lists'=>'lists#create'
+  get 'lists'=>'lists#index'
   get 'lists/edit'
   get'/top'=>'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get'lists/:id'=>'lists#show',as:"list"
 end
