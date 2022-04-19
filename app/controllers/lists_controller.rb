76065e84cc4182bc1ea171dd90249405c652469d
EC2 Default User <ec2-user@ip-172-31-85-166.ec2.internal>
2022-04-19 11:13:14 +0000

    Initial Commit3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -19,6 +19,7 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list=List.find(params[:id])
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
   post 'lists'=>'lists#create'
-  get 'lists'=>'lists#index'
-  get 'lists/edit'
+  get'lists'=>'lists#index'
   get'/top'=>'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get'lists/:id'=>'lists#show',as:"list"
+  get'lists/:id/edit'=>'lists#edit',as:'edit_list'
 end
