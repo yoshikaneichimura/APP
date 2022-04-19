d715ec7d6e3692d7058e34e43a64152b479b7721
EC2 Default User <ec2-user@ip-172-31-85-166.ec2.internal>
2022-04-19 14:52:19 +0000

    Initial Commit4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,9 +22,21 @@ class ListsController < ApplicationController
     @list=List.find(params[:id])
   end
   
+  def update
+    list=List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destory
+   list=List.find(params[:id])
+   list.destroy
+   redirect_to'/lists'
+  end
+  
   private
   
    def list_params
-     params.require(:list).permit(:title,:body)
+     params.require(:list).permit(:title,:body,:image)
    end
 end
